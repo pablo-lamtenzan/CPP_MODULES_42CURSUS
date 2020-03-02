2179225f79f385f15630586519f975c9c7d92806
plamtenz <plamtenz@student.le-101.fr>
2020-03-03 00:41:30 +0100

    mod1 ex00

--- module01/ex00/main.cpp
+++ module01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/03 00:12:03 by plamtenz          #+#    #+#             */
-/*   Updated: 2020/03/03 00:29:11 by plamtenz         ###   ########.fr       */
+/*   Updated: 2020/03/03 00:40:45 by plamtenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int main()
 {
+    std::string something;
+    std::cout << "Welcome to my Pony creator, please write something for continue.\n";
+    std::cin >> something;
+    std::cout << "Lets Allocate some Pony, please write something for continue.\n";
+    std::cin >> something;
     ponyOnTheHeap();
+    std::cout << "Lets Stack some Pony, please write something for continue.\n";
+    std::cin >> something;
+    ponyOnTheStack();
 }
