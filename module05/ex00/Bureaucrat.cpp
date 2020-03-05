9df11604963b71263d88701f6884515968b5d6d4
plamtenz <plamtenz@student.le-101.fr>
2020-03-05 09:36:33 +0100

    dont know if the result is what i need to have but it compiles xdddd

---
+++ module05/ex00/Bureaucrat.cpp
@@ -0,0 +1,168 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 08:38:20 by plamtenz          #+#    #+#             */
+/*   Updated: 2020/03/05 09:32:58 by plamtenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+
+// Constructors
+
+Bureaucrat::Bureaucrat()
+{
+    return ;
+}
+
+Bureaucrat::Bureaucrat(std::string name, int  grade)
+{
+    this->__name = name;
+    this->__grade = grade;
+    return ;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+{
+    *this = src;
+    return ;
+}
+
+HIGHT::GradeTooHighException(int grade) : __grade(grade)
+{
+    
+}
+
+HIGHT::GradeTooHighException(const HIGHT &src) : __grade(src.__grade)
+{
+    
+}
+
+LOW::GradeToolowException(int grade) : __grade(grade)
+{
+    
+}
+
+LOW::GradeToolowException(const LOW &src) : __grade(src.__grade)
+{
+    
+}
+
+// Destructors
+
+Bureaucrat::~Bureaucrat()
+{
+    return ;
+}
+
+HIGHT::~GradeTooHighException() throw()
+{
+    
+}
+
+LOW::~GradeToolowException() throw()
+{
+    
+}
+
+// Operators
+
+Bureaucrat
+&Bureaucrat::operator= (const Bureaucrat &src)
+{
+    if (this == &src)
+    {
+        this->__name = src.__name;
+        this->__grade = src.__grade;
+    }
+    return (*this);
+}
+
+std::ostream
+&operator<< (std::ostream &out, const Bureaucrat &src)
+{
+    out << '<' << src.getName() << ">, bureaucrat grade <" << src.getGrade() << ">\n";
+    return (out);
+}
+
+HIGHT
+&HIGHT::operator= (const HIGHT &src)
+{
+    if (this != &src)
+        this->__grade = src.__grade;
+    return (*this);
+}
+
+LOW
+&LOW::operator= (const LOW &src)
+{
+    if (this != &src)
+        this->__grade = src.__grade;
+    return (*this);
+}
+
+// Methods
+
+std::string
+Bureaucrat::getName() const
+{
+    return (this->__name);
+}
+
+int
+Bureaucrat::getGrade() const
+{
+    return (this->__grade);
+}
+
+void
+Bureaucrat::CheckExeption(int grade)
+{
+    if (grade < 1)
+        throw GradeTooHighException(grade);
+    if (grade > 150)
+        throw GradeToolowException(grade);
+    this->__grade = grade;
+}
+
+void
+Bureaucrat::IncrementGrade()
+{
+    this->CheckExeption(this->__grade - 1);
+    return ;
+}
+
+void
+Bureaucrat::DecrementGrade()
+{
+    this->CheckExeption(this->__grade + 1);
+    return ;
+}
+
+int
+HIGHT::getGrade() const
+{
+    return (this->__grade);
+}
+
+const std::string
+HIGHT::TooHigh() const throw()
+{
+    return ("Grade is too high\n");
+}
+
+int
+LOW::getGrade() const
+{
+    return (this->__grade);
+}
+
+const std::string
+LOW::TooLow() const throw()
+{
+    return ("Grade is too low\n");
+}
---
+++ module05/ex00/Bureaucrat.hpp
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/03/05 08:11:52 by plamtenz          #+#    #+#             */
+/*   Updated: 2020/03/05 09:21:34 by plamtenz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+#define HIGHT Bureaucrat::GradeTooHighException
+#define LOW Bureaucrat::GradeToolowException
+
+class Bureaucrat
+{
+    private :
+
+    std::string __name;
+    int __grade;
+
+    public :
+
+    // Constructors
+    Bureaucrat();
+    Bureaucrat(std::string name, int grade);
+    Bureaucrat(const Bureaucrat &src);
+
+    // Destructors
+    ~Bureaucrat();
+
+    // Operators
+    Bureaucrat &operator= (const Bureaucrat &src);
+
+    // Shared Methods
+    std::string getName() const;
+    int getGrade() const;
+    void CheckExeption(int grade);
+    void IncrementGrade();
+    void DecrementGrade();
+
+    // Shared Sub-Classes
+
+    class GradeTooHighException : public std::exception
+    {
+        private :
+
+        int __grade;
+        
+        public :
+
+        // Constructors
+        GradeTooHighException(int grade);
+        GradeTooHighException(const GradeTooHighException &src);
+
+        // Destructors
+        ~GradeTooHighException() throw();
+
+        // Operators
+        GradeTooHighException &operator= (const GradeTooHighException &src);
+
+        // Shared Methods
+        int getGrade() const;
+        const std::string TooHigh() const throw();
+    };
+
+    class GradeToolowException : public std::exception
+    {
+        private :
+
+        int __grade;
+    
+        public :
+
+        // Constructors
+        GradeToolowException(int grade);
+        GradeToolowException(const GradeToolowException &src);
+        
+        // Destructors
+        ~GradeToolowException() throw();
+
+        // Operators
+        GradeToolowException &operator= (const GradeToolowException &src);
+
+        // Shared Methods
+        int getGrade() const;
+        const std::string TooLow()const throw();
+    };
+};
+
+std::ostream &operator<< (std::ostream &out, Bureaucrat const &src);
+
+#endif
