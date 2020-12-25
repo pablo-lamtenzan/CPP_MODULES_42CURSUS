425cfa4d9a9bda100e18acc6cbfe1703567b1952
Pablo Lamtenzan <pablolamtenzan@gmail.com>
2020-12-25 18:57:58 +0100

    01 done

--- module06/ex01/main.cpp
+++ module06/ex01/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: plamtenz <plamtenz@student.42.fr>          +#+  +:+       +#+        */
+/*   By: pablo <pablo@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/03/06 14:19:28 by plamtenz          #+#    #+#             */
-/*   Updated: 2020/03/08 19:30:08 by plamtenz         ###   ########.fr       */
+/*   Updated: 2020/12/25 18:54:58 by pablo            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,45 +15,54 @@
 #include <cstdlib>
 #include <random>
 
-typedef struct data
+typedef struct	data
 {
     std::string s1;
     int n;
     std::string s2;
 }               Data;
 
-void *serialize(void)
+void*		serialize(void)
 {
-    // random generator init
+	static const char alnum[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
     std::srand(time(NULL));
 
-    // dest, src
     char *raw;
-    if (!(raw = new char[sizeof(char) * 16 + sizeof(int)]))
-        return (NULL);
-    char alnum[63] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int i = -1;
-    // 1st 8 bits (char * 8)
-    while (++i < 8)
-        raw[i] = alnum[rand() % 62];
-    // 2nd 16 bits (int * 4)
-    *reinterpret_cast<int *>(raw + 8) = rand();
-    i = -1;
-    while (++i < 8)
-        raw[i + sizeof(char)  8 + sizeof(int)] = alnum[rand() % 62];
+
+	// Allocate the total memory room
+	try {
+		raw = new char[sizeof(char) * 16ul + sizeof(int)];
+	} catch(const std::bad_alloc& e) { std::cerr << e.what() << std::endl; return (NULL); }
+
+	// Fill the first 8 bits with random alphanumeric chars
+	for (size_t i = 0ul ; i < 8ul ; i++)
+		raw[i] = alnum[rand() % (sizeof(alnum) - 1ul)];
+
+    // Fill bit 8 to bit 8 + sizeof(int) with a random integer
+    *reinterpret_cast<int *>(raw + 8ul) = rand();
+
+    // Fill the remaining last 8 bits with 8 random alpahnumeric chars
+    for (size_t i = 8ul + sizeof(int) ; i < 8ul + sizeof(int) + 8ul ; i++)
+		raw[i] = alnum[rand() % (sizeof(alnum) - 1ul)];
     return (raw);
 }
 
 Data *deserialize(void * raw)
 {
     Data *data;
-    if (!(data = new Data()))
-        return (NULL);
-    char *ptr = reinterpret_cast<char *>(raw);
-    // fill data in struct
-    data->s1 = std::string(ptr, 8);
-    data->n = *reinterpret_cast<int *>(ptr + 8);
-    data->s2 = std::string(ptr + sizeof(char) * 8 + sizeof(int), 8);
+
+	// Allocate the deseralise struct
+	try {
+		data = new Data();
+	} catch (const std::exception& e) { std::cerr << e.what() << std::endl; return (NULL); }
+
+    char *ptr = reinterpret_cast<char*>(raw);
+
+    // Fill each elem of the struct with their restective data chunk
+    data->s1 = std::string(ptr, 8ul);
+    data->n = *reinterpret_cast<int*>(ptr + 8ul);
+    data->s2 = std::string(ptr + sizeof(char) * 8ul + sizeof(int), 8ul);
     return (data);
 }
 
@@ -61,21 +70,31 @@ int main()
 {
     void *raw;
     Data *data;
+
     if (!(raw = serialize()) || !(data = deserialize(raw)))
-    {
-        std::cout << "Memory allocation failed" << std::endl;
-        return (0);
-    }
-    unsigned char *cp = reinterpret_cast<unsigned char *>(raw);
-    std::cout << "Serialize data" << std::endl;
-    int i = -1;
-    while (++i < 16 + sizeof(int))
-        std::cout << std::hex << std::showbase << + cp[i] << " ";
-    std::cout << std::endl << std::endl;
-    std::cout << "Deserialized data:" << std::endl;
+		return (1);
+
+    unsigned char *cp = reinterpret_cast<unsigned char*>(raw);
+
+	
+    std::cout << "Serialized data:" << std::endl << std::endl << "\"";
+
+	for (size_t i = 0ul ; i < 8ul ; i++)
+		std::cout << cp[i];
+	std::cout << *reinterpret_cast<int*>(cp + 8ul);
+	for (size_t i = 8ul + sizeof(int) ; i < 8ul + sizeof(int) + 8ul ; i++)
+		std::cout << cp[i];
+	
+	std::cout << "\"" << std::endl << std::endl << "------------------------" << std::endl << std::endl;
+
+    std::cout << "Deserialized data:" << std::endl << std::endl;
+
 	std::cout << "s1: \"" << data->s1 << "\"" << std::endl;
 	std::cout << "n:  " << std::dec << data->n << std::endl;
 	std::cout << "s2: \"" << data->s2 << "\"" << std::endl;
-    delete cp;
+
+    delete [] cp;
     delete data;
+	std::cout << "EXIT" << std::endl;
+	return (0);
 }
